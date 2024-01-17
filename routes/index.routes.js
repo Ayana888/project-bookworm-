8756e5125d055b6d4aa83c9a3840d0e7356127f0
Ayana888 <abbolotova2024@gmail.com>
2024-01-17 19:22:40 +0300

    addBook

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,10 +3,12 @@ const mainPageRouter = require("./views/main.routes");
 const authRouter = require("./views/auth.routes");
 
 const apiAuthRouter = require("./api/api.auth.routes");
+// const apiBooksRouter = require("./api/api.books.routes");
 
 router.use("/", mainPageRouter);
 router.use("/auth", authRouter);
 
 router.use("/api/auth", apiAuthRouter);
+// router.use("/api/books", apiBooksRouter);
 
 module.exports = router;
