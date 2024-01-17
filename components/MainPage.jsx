b2971833d63e9b99072f2f208689acd6aabf9a5a
Nikita <nik.aksenovv@gmail.com>
2024-01-17 18:11:54 +0300

    MainPage

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -1,7 +1,8 @@
 const React = require("react");
 const Layout = require("./Layout");
+const BookItem = require("./BookItem");
 
-function MainPage({ title, user }) {
+function MainPage({ title, user, book }) {
   return (
     <Layout title={title} user={user}>
       <div
@@ -11,7 +12,13 @@ function MainPage({ title, user }) {
           justifyContent: "space-between",
         }}
       >
-        <h1>Main page</h1>
+        <h1></h1>
+        <div className="container book-container" style={{display:'flex', justifyContent:'center'}}>
+        {book.map((books) => (
+          <BookItem key={books.id} book={books} />
+          
+        ))}
+        </div>
       </div>
     </Layout>
   );
