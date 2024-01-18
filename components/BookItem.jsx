a27e224b59c3b17c5cf1d750e289f08951454c87
Ayana888 <abbolotova2024@gmail.com>
2024-01-18 16:20:07 +0300

    update

--- components/BookItem.jsx
+++ components/BookItem.jsx
@@ -1,10 +1,9 @@
-const React = require('react');
-const Rate = require('./Rate');
-
+const React = require("react");
+const Rate = require("./Rate");
 
 function BookItem({ book, user }) {
   //console.log(book);
-  const [like] = book.Likes.filter((el)=>el.user_id === user.id);
+  // const [like] = book.Likes.filter((el) => el.user_id === user.id);
 
   return (
     <div style={{ display: "flex", paddingRight: "90px" }}>
@@ -13,28 +12,47 @@ function BookItem({ book, user }) {
         <div className="card-body">
           <h5 className="card-title">{book.name}</h5>
           <p className="card-text">{book.author}</p>
-        
- <a href={`/books/${book.id}`} className="btn btn-primary">Подробнее</a>
 
-        {like ? (
-          <button data-id={book.id} className="btn btn-success dfavorite" type="button">
-            Удалить из избранного
-          </button>
-        ) : (
-          <button data-id={book.id} className="btn btn-success favorite" type="button">
-            В избранное
-          </button>
-        )}
-         
-    <button data-id={book.id}  className="btn btn-danger delete" type="button">Удалить</button>
-        <a href={`/heroes/update-form/${book.id}`} className="btn btn-warning update" type="button">
-             Изменить
-        </a> 
+          <a href={`/books/${book.id}`} className="btn btn-primary">
+            Подробнее
+          </a>
 
+          {/* {like ? (
+            <button
+              data-id={book.id}
+              className="btn btn-success dfavorite"
+              type="button"
+            >
+              Удалить из избранного
+            </button>
+          ) : (
+            <button
+              data-id={book.id}
+              className="btn btn-success favorite"
+              type="button"
+            >
+              В избранное
+            </button>
+          )} */}
+
+          <button
+            data-id={book.id}
+            className="btn btn-danger delete"
+            type="button"
+          >
+            Удалить
+          </button>
+          <a
+            href={`/books/update-form/${book.id}`}
+            className="btn btn-warning update"
+            type="button"
+          >
+            Изменить
+          </a>
         </div>
       </div>
     </div>
   );
-
+}
 
 module.exports = BookItem;
