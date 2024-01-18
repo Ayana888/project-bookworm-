996725af13c5a17a4767113c6421d0c25ce47468
Ayana888 <abbolotova2024@gmail.com>
2024-01-18 17:36:25 +0300

    update

--- components/BookItem.jsx
+++ components/BookItem.jsx
@@ -1,11 +1,8 @@
-
-
 const React = require("react");
 const Rate = require("./Rate");
 
 function BookItem({ book, user }) {
   //console.log(book);
-
 
   return (
     <div style={{ display: "flex", paddingRight: "90px" }}>
@@ -14,22 +11,27 @@ function BookItem({ book, user }) {
         <div className="card-body">
           <h5 className="card-title">{book.name}</h5>
           <p className="card-text">{book.author}</p>
-    <a href={`/books/${book.id}`} className="btn btn-primary">
+          <a href={`/books/${book.id}`} className="btn btn-primary">
             Подробнее
           </a>
 
-        {book.Likes ? (
-          <button data-id={book.id} className="btn btn-success dfavorite" type="button">
-            Удалить из избранного 💔
-          </button>
-        ) : (
-          <button data-id={book.id} className="btn btn-success favorite" type="button">
-            В избранное ❤️
-          </button>
-        )}
-         
-    <button data-id={book.id}  className="btn btn-danger delete" type="button">Удалить</button>
-       
+          {book.Likes ? (
+            <button
+              data-id={book.id}
+              className="btn btn-success dfavorite"
+              type="button"
+            >
+              Удалить из избранного 💔
+            </button>
+          ) : (
+            <button
+              data-id={book.id}
+              className="btn btn-success favorite"
+              type="button"
+            >
+              В избранное ❤️
+            </button>
+          )}
 
           <button
             data-id={book.id}
@@ -50,8 +52,7 @@ function BookItem({ book, user }) {
     </div>
   );
 
-      //  }
-
+  //  }
 }
 
 module.exports = BookItem;
